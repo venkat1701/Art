54581cd9363c1741cb28293cda9f0b2de4df1ea8
Venkt <87254400+Venkat1701@users.noreply.github.com>
2022-05-29 12:48:24 +0530

    Minor Updates

---
+++ sketch.js
@@ -0,0 +1,9 @@
+let open = false;
+let hammenu = document.querySelector(".header .hammenu");
+let sidenav = document.querySelector(".sidebar");
+
+
+function openCloseHam(){
+    hammenu.classList.toggle("change");
+    
+}
